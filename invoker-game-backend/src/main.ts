770e554b232b03cbc2fe5c0c4f888f62e244b79f
Dmitry Moiseenko <majest228@gmail.com>
2024-11-26 21:45:09 +0500

    Update main.ts

--- invoker-game-backend/src/main.ts
+++ invoker-game-backend/src/main.ts
@@ -4,9 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api-v1');
-  app.enableCors({
-    origin: 'http://localhost:5173',
-  });
+
+  const allowedOrigins = [
+    'http://localhost:5173',
+    'https://invoker-mastery.vercel.app',
+  ];
+  app.enableCors({ credentials: true, origin: allowedOrigins });
   await app.listen(7777);
 }
 bootstrap();
