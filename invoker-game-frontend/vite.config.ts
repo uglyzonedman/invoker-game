21968c4fff2f06a6a2a0154f92c80ad7ccecf62a
Dmitry Moiseenko <majest228@gmail.com>
2024-11-26 21:56:16 +0500

    Update vite.config.ts

--- invoker-game-frontend/vite.config.ts
+++ invoker-game-frontend/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	server: {
+		port: 5173,
+		host: '0.0.0.0', // чтобы приложение было доступно извне
+	},
 })
