bdfd752c7f89cdbccb1cba5eb207725a9338a5d5
Dmitry Moiseenko <majest228@gmail.com>
2024-11-26 22:06:32 +0500

    Update main.ts

--- invoker-game-backend/src/main.ts
+++ invoker-game-backend/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
     'https://invoker-mastery.vercel.app',
     'http://85.159.231.74:4174',
   ];
-  app.enableCors({ credentials: true, origin: allowedOrigins });
+  app.enableCors({ origin: allowedOrigins });
   await app.listen(7777);
 }
 bootstrap();
